b8b5bf7e132d7a732d3b31dc90202fcfee0f6aac
matorolll <mgajos00@gmail.com>
2023-08-01 15:26:41 +0200

    Added slider and sqare size change

--- WallpaperSlicer.py
+++ WallpaperSlicer.py
@@ -5,6 +5,50 @@ from PIL import Image, ImageTk
 from tkinter import filedialog
 
 
+class MyDialog(tkinter.simpledialog.Dialog):
+    def __init__(self, parent, title):
+        self.my_username = None
+        self.my_password = None
+        super().__init__(parent, title)
+
+    def body(self, frame):
+        # print(type(frame)) # tkinter.Frame
+        self.my_username_label = tkinter.Label(frame, width=25, text="Username")
+        self.my_username_label.pack()
+        self.my_username_box = tkinter.Entry(frame, width=25)
+        self.my_username_box.pack()
+
+        self.my_password_label = tkinter.Label(frame, width=25, text="Password")
+        self.my_password_label.pack()
+        self.my_password_box = tkinter.Entry(frame, width=25)
+        self.my_password_box.pack()
+        self.my_password_box['show'] = '*'
+
+        return frame
+
+    def ok_pressed(self):
+        # print("ok")
+        self.my_username = self.my_username_box.get()
+        self.my_password = self.my_password_box.get()
+        self.destroy()
+
+    def cancel_pressed(self):
+        # print("cancel")
+        self.destroy()
+
+
+    def buttonbox(self):
+        self.ok_button = tkinter.Button(self, text='OK', width=5, command=self.ok_pressed)
+        self.ok_button.pack(side="left")
+        cancel_button = tkinter.Button(self, text='Cancel', width=5, command=self.cancel_pressed)
+        cancel_button.pack(side="right")
+        self.bind("<Return>", lambda event: self.ok_pressed())
+        self.bind("<Escape>", lambda event: self.cancel_pressed())
+
+def mydialog(app):
+    dialog = MyDialog(title="Login", parent=app)
+    return dialog.my_username, dialog.my_password
+
 
 class Square:
     def __init__(self, canvas, x, y, width, height, color, tag, zoom_factor):
@@ -19,6 +63,13 @@ class Square:
 
         self.shape = self.canvas.create_rectangle(x, y, x + (width * zoom_factor) , y + (height * zoom_factor), fill=color, tag=tag)
         self.canvas.tag_bind(self.shape, '<B1-Motion>', self.move)
+        self.canvas.tag_bind(self.shape, '<Double-Button-1>', self.on_double_click)
+
+
+
+    def update(self):
+        self.canvas.coords(self.shape, self.x, self.y, self.x + (self.width * self.zoom_factor), self.y + (self.height * self.zoom_factor))
+
 
     def move(self, event):
         dx = (event.x - self.x - (self.width * self.zoom_factor) // 2)
@@ -36,12 +87,16 @@ class Square:
         self.canvas.coords(self.shape, self.x, self.y, self.x + (self.width * zoom_factor),  self.y + (self.height * zoom_factor))
 
 
+    def on_double_click(self, event):
+        app.on_double_click_create_app_boxes(self)
+
 
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
         self.title("Wallpaper Spacer")
-        self.geometry(f"{1100}x{580}")
+        self.geometry(f"{1100}x{700}")
+
 
         # configure grid layout (4x4)
         self.grid_columnconfigure(1, weight=1)
@@ -64,21 +119,59 @@ class App(customtkinter.CTk):
         self.checkbox_slider_frame = customtkinter.CTkFrame(self.sidebar_frame)
         self.checkbox_slider_frame.grid(row=4, column=0, padx=(20, 20), pady=(20, 0), sticky="nsew")
 
-        self.canvas = customtkinter.CTkCanvas(self, height=700, bg="white")
+        self.canvas = customtkinter.CTkCanvas(self, height=1000, bg="white")
         self.canvas.grid(row=0, column=1, padx=(20, 20), pady=(20, 20), sticky="nsew")
 
         self.canvas.bind("<MouseWheel>", self.zoom_event)
-
         self.squares = []
-
-
         self.zoom_factor = .2
-
-        self.zoom_label = customtkinter.CTkLabel(self.sidebar_frame, text=f"Zoom Level: {self.zoom_factor*100}", font=customtkinter.CTkFont(size=12))
+        self.zoom_label = customtkinter.CTkLabel(self.sidebar_frame, text=f"Zoom Level: {self.zoom_factor*100}%", font=customtkinter.CTkFont(size=12))
         self.zoom_label.grid(row=5, column=0, padx=20, pady=(20, 10))
+      
 
 
+    def on_double_click_create_app_boxes(self,square): 
+        self.old_width = square.width
+        self.old_height = square.height
 
+        self.edit_width_slider_text = customtkinter.CTkLabel(self.sidebar_frame, text=f"Width: {square.width}", font=customtkinter.CTkFont(size=20, weight="bold"))
+        self.edit_width_slider_text.grid(row=6, column=0, padx=(20, 10), pady=(10, 10), sticky="ew")
+        self.edit_width_slider = customtkinter.CTkSlider(self.sidebar_frame,from_=0, to=1, number_of_steps=9, command=lambda value: self.change_square_size(square,"width"))
+        self.edit_width_slider.grid(row=7, column=0, padx=(20, 10), pady=(10, 10), sticky="ew")
+
+        self.edit_height_slider_text = customtkinter.CTkLabel(self.sidebar_frame, text=f"Height: {square.height}", font=customtkinter.CTkFont(size=20, weight="bold"))
+        self.edit_height_slider_text.grid(row=8, column=0, padx=(20, 10), pady=(10, 10), sticky="ew")
+        self.edit_height_slider = customtkinter.CTkSlider(self.sidebar_frame,from_=0, to=1, number_of_steps=4,  command=lambda value: self.change_square_size(square,"height"))
+        self.edit_height_slider.grid(row=9, column=0, padx=(20, 10), pady=(10, 10), sticky="ew") 
+
+        self.edit_size_slider_text = customtkinter.CTkLabel(self.sidebar_frame, text=f"Both: {square.width}x{square.height}", font=customtkinter.CTkFont(size=20, weight="bold"))
+        self.edit_size_slider_text.grid(row=10, column=0, padx=(20, 10), pady=(10, 10), sticky="ew")
+        self.edit_size_slider = customtkinter.CTkSlider(self.sidebar_frame,from_=0, to=1, number_of_steps=4,  command=lambda value: self.change_square_size(square,"both"))
+        self.edit_size_slider.grid(row=11, column=0, padx=(20, 10), pady=(10, 10), sticky="ew") 
+
+
+    def change_square_size(self,square,scalling):
+        if(scalling == "width"):
+            square.width = int(self.edit_width_slider.get() * (self.old_width+self.old_width))
+            self.edit_width_slider_text.configure(text=f"Width: {square.width}")
+        if(scalling == "height"):
+            square.height = int(self.edit_height_slider.get() * (self.old_height+self.old_height))
+            self.edit_height_slider_text.configure(text=f"Height: {square.height}")
+        if(scalling == "both"):
+            square.width = int(self.edit_size_slider.get() * (self.old_width+self.old_width))
+            square.height = int(self.edit_size_slider.get() * (self.old_height+self.old_height))
+            self.edit_size_slider_text.configure(text=f"Both: {square.width}x{square.height}")
+        square.update()
+
+
+    
+
+
+    def show_square_data(self, square):
+        self.square_data_label.configure(text=f"Selected Square: Width: {square.width}, Height: {square.height}")
+
+    def update_square_label(self, square):
+        self.show_square_data(square)
 
     def getting_image_data(self):
         file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.png;*.jpg;*.jpeg;*.gif")])
@@ -98,30 +191,27 @@ class App(customtkinter.CTk):
         self.apply_zoom_img()
         self.apply_zoom_square()
 
-
     def apply_zoom_square(self):
         for square in self.squares:
             square.set_zoom_factor(self.zoom_factor)
 
-
     def apply_zoom_img(self):
-        if self.image_item is not None:
-            width = int(self.image.width * self.zoom_factor)
-            height = int(self.image.height * self.zoom_factor)
-            resized_image = self.image.resize((width, height), Image.ANTIALIAS)
-            self.image_tk = ImageTk.PhotoImage(resized_image)
-            self.canvas.itemconfig(self.image_item, image=self.image_tk)
-            self.canvas.config(scrollregion=self.canvas.bbox("all"))
-
-            # Update the zoom level label
-            zoom_percentage = int(self.zoom_factor * 100)
-            self.zoom_label.configure(text=f"Zoom Level: {zoom_percentage}%")
-
+        try:
+            if self.image_item is not None:
+                width = int(self.image.width * self.zoom_factor)
+                height = int(self.image.height * self.zoom_factor)
+                resized_image = self.image.resize((width, height), Image.ANTIALIAS)
+                self.image_tk = ImageTk.PhotoImage(resized_image)
+                self.canvas.itemconfig(self.image_item, image=self.image_tk)
+                self.canvas.config(scrollregion=self.canvas.bbox("all"))
+                zoom_percentage = int(self.zoom_factor * 100)
+                self.zoom_label.configure(text=f"Zoom Level: {zoom_percentage}%",text_color='white')
+        except AttributeError:
+            self.zoom_label.configure(text="Need to choose img first in order to zoom!", font=customtkinter.CTkFont(size=12), text_color='red')
 
     def getting_screen_data(self):
         import gettingsScreens
         listOfMonitors = gettingsScreens.get_screen_info()
-
         self.checkboxes = []
         for i, monitors in enumerate(listOfMonitors):
             checkbox_name = f"checkbox_{i+1}"
@@ -129,7 +219,6 @@ class App(customtkinter.CTk):
             checkbox.grid(row=i+3, column=0, pady=(20, 0), padx=20, sticky="n")
             self.checkboxes.append(checkbox)   
             checkbox.configure(command=lambda checkbox=checkbox, name=monitors[1], width=monitors[2], height=monitors[3]: self.create_or_delete_rectangle(checkbox, name, width, height))
-
 
     def create_or_delete_rectangle(self,checkbox,name,width,height):
         if checkbox.get():
